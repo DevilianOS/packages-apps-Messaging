AOSP-Platform/Messaging

AOSP/Messaging - Moved LOCAL_COMPATIBILITY_SUITE to tests/Android.mk. + Deleted the unnecessary blankId check. Updated the TODO comment for NUM_TEST_CONVERSATIONS+4. + Added AndroidTest.xml + Added "LOCAL_PROGUARD_ENABLED := disabled" for tests to pass on cf.
Bug: 126516784

Test: make messagingtests and tested the messaging UI manually. Ran the following CTS tests on Pixel phone.

$ make -j 40
  -rw-r--r-- 1 rtenneti primarygroup 8529760 Mar  8 16:46 out/target/product/marlin/system/app/messaging/messaging.apk

$ make messagingtests -j
  -rw-r--r-- 1 rtenneti primarygroup 729713 Mar 13 14:22 out/target/product/marlin/testcases/messagingtests/messagingtests.apk

$ adb install -r -d out/target/product/marlin/system/app/messaging/messaging.apk
$ adb install -r -d out/target/product/marlin/testcases/messagingtests/messagingtests.apk

atest
-----
$ cd .../packages/apps/Messaging
$ atest
  ...
  Results from tests that require device:

  Summary
  -------
  messagingtests: Passed: 113, Failed: 0, Ignored: 0

  All tests passed!

acloud test
-----------
$ lunch aosp_cf_x86_phone-userdebug

$ make -j 40
  -rw-r--r-- 1 rtenneti primarygroup 10585182 Mar 15 15:19 ./target/product/vsoc_x86/system/app/messaging/messaging.apk
$ make messagingtests -j
  -rw-r--r-- 1 rtenneti primarygroup 643697 Mar 15 15:57 ./target/product/vsoc_x86/system/app/messagingtests/messagingtests.apk

$ acloud reconnect
$ adb devices
  List of devices attached
  127.0.0.1:52761	device

$ adb install -r -d out/target/product/vsoc_x86/system/app/messaging/messaging.apk
$ adb install -r -d out/target/product/vsoc_x86/system/app/messagingtests/messagingtests.apk

$ cd packages/apps/Messaging
$ atest messagingtests -- --abi x86 --serial 127.0.0.1:52761

Change-Id: I6b1143db6c596805f3c48f310323e0e02d344d00

tests/Android.mk
@@ -24,8 +24,12 @@ LOCAL_PACKAGE_NAME := messagingtests
 
 LOCAL_INSTRUMENTATION_FOR := messaging
 
+LOCAL_PROGUARD_ENABLED := disabled
+
 # Matching ../Android.mk
 LOCAL_SDK_VERSION := current
+
+LOCAL_COMPATIBILITY_SUITE := device-tests
 
 LOCAL_CERTIFICATE := platform
 

DevilianOS/packages-apps-Messaging

tests/Android.mk
@@ -24,8 +24,12 @@ LOCAL_PACKAGE_NAME := messagingtests
 
 LOCAL_INSTRUMENTATION_FOR := messaging
 
+LOCAL_PROGUARD_ENABLED := disabled
+
 # Matching ../Android.mk
 LOCAL_SDK_VERSION := current
+
+LOCAL_COMPATIBILITY_SUITE := general-tests
 
 LOCAL_CERTIFICATE := platform
 